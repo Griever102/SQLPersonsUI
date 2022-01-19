9f3230aeeed895cf3aa0b3adfcfd0d07c79fc640
Justin Hart <jhart91990@gmail.com>
2022-01-19 16:14:38 -0700

    Add project files.

---
+++ PersonsFormUI/Data.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PersonsFormUI
+{
+    class Data
+    {
+        public int ID { get; set; }
+        public int PersonID { get; set; }
+        public string EmailAddress { get; set; }
+        public string PhoneNumber { get; set; }
+    }
+}
---
+++ PersonsFormUI/Person.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PersonsFormUI
+{
+    public class Person
+    {
+        public int Id { get; set; }
+        public string Fristname { get; set; }
+        public string LastName { get; set; }
+    }
+}
