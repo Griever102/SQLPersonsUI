9945067af38d76436ea277e6f81aa798cf4e892a
Justin Hart <jhart91990@gmail.com>
2022-01-20 17:42:48 -0700

    Combined PersonAddress and Data into Person

--- PersonsFormUI/Classes/Person.cs
+++ PersonsFormUI/Classes/Person.cs
@@ -11,13 +11,38 @@ namespace PersonsFormUI
         public int Id { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
+        public int ID { get; set; }
+        public int PersonID { get; set; }
+        public string Address { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public string ZipCode { get; set; }
+        //public int PersonID { get; set; }
+        public string EmailAddress { get; set; }
+        public string PhoneNumber { get; set; }
+
+        public string FullData
+        {
+            get
+            {
+                return $"{ EmailAddress } { PhoneNumber }";
+            }
+        }
+
+        public string FullPersonAddress
+        {
+            get
+            {
+                return $"{ Address } { City } { State } { ZipCode }";
+            }
+        }
 
         public string FullPerson
         {
             get 
             {
                     // FirstName LastName
-                return $"{ FirstName } { LastName }";
+                return $"{ FirstName } { LastName } { PhoneNumber } { EmailAddress }";
             }
         }
 
